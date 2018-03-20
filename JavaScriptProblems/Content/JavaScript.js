7fc03a188eb489f081254cf4ffa6f9a0cabd40ac
JuliePretzlaff <juliapretzlaff@gmail.com>
2018-03-20 17:04:53 -0400

    added onfocus and onblur

--- JavaScriptProblems/Content/JavaScript.js
+++ JavaScriptProblems/Content/JavaScript.js
@@ -16,3 +16,17 @@ function alphOrder() {
     var var2 = userResponse.split('').sort().join('').toString();
     alert(var2);
 }
+
+//scroll event test
+function scrollTest(x) {
+    x.getElementsByTagName("P")[1].style.backgroundColor = "blue";
+}
+
+
+//form focus test
+function colorChange(x) {
+        x.style.border = "thick solid lightblue";
+}
+function colorNormal(x) {
+    x.style.border = "thin solid lightgrey";
+}
