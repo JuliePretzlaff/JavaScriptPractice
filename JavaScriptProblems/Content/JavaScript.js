6166fcfa2f807fecb78e79ce6499b5b131131831
JuliePretzlaff <juliapretzlaff@gmail.com>
2018-03-20 14:45:54 -0400

    first two problems

---
+++ JavaScriptProblems/Content/JavaScript.js
@@ -0,0 +1,18 @@
+﻿//question 1:
+function checkPalindrome() {
+    var userResponse = prompt('word')
+    var var2 = userResponse.split('').reverse().join('');
+    if (userResponse === var2) {
+        alert('This is a palindrome');
+    }
+    else {
+        alert('This is NOT a palindrome');
+    }
+}
+
+//question 2:
+function alphOrder() {
+    var userResponse = prompt('word')
+    var var2 = userResponse.split('').sort().join('').toString();
+    alert(var2);
+}
---
+++ JavaScriptProblems/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JavaScriptProblems.MvcApplication" Language="C#" %>
